c18cce70bcc8d1ab6b983e14d64cae5b266d0c6f
grant chichester <gchichester@gmail.com>
2022-11-18 09:20:13 +0100

    backend portion created

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,43 @@
+class BookmarksController < ApplicationController
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    set_list
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    set_list
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    set_bookmark
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+end
